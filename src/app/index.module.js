9cd880a2fc7855a3cb57978789eb4895a6150bdd
Daniel Yim <danielyim@gmail.com>
2016-01-25 23:33:41 -0600

    initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gerbsDawg', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr', 'firebase']);
+
+})();
